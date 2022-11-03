e07b300e07c6dcfe65be541829c6e7c84a997563
KritikaP1997 <kritikagaur941@gmail.com>
2022-11-03 18:54:29 +0530

    chat app using node, socket.io, html, css

---
+++ js/client.js
@@ -0,0 +1,51 @@
+const socket = io('http://localhost:8000');
+
+//get DOM element in respctive js Variable
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.container');
+
+//Audio  that will play on receiving message..
+// var audio = new Audio('MessageTone.mp3')
+
+// function which will append event info to the container
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    // if (position == 'left') { audio.play(); };
+}
+
+
+//Ask new user for her/his name and let the server know
+const name = prompt("Enter your Name..");
+socket.emit('new-user-joined', name);
+
+//if the new user joins, receive his/her name from the server
+socket.on('user-joined', name => {
+    console.log(name, "namenamename")
+    append(`${name} joined the chat`, 'right')
+});
+
+//if server send a message, receive it.
+socket.on('receive', data => {
+    console.log(data, "datadata")
+    append(`${data.name}: ${data.message}`, 'left')
+});
+
+//if the user leaves the chat, append the info to the container
+socket.on('left', name => {
+    append(`${name} left the chat`, 'right  ')
+});
+
+//if the form gets submitted, send server the message
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message)
+    messageInput.value = ''
+})
+
---
+++ nodeCode/index.js
@@ -0,0 +1,37 @@
+// node server which will handle socket connections
+const express = require("express");
+const cors = require('cors');
+
+const app = express();
+var io = require("socket.io")(8000, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+const users = {};
+
+io.on('connection', socket => {
+
+    //If any new user join, let other users connected to the server know!
+    socket.on('new-user-joined', name => {
+
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+
+    });
+
+    // is someone send message, broadcast it to other people
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+    });
+
+    // if someone leave the chat, let others know!
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id])
+        delete users[socket.id];
+    });
+
+
+});
